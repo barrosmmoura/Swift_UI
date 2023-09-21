da9d080cc2ea8405162667a9eae2431c14684683
Mariana Barros <barrosmmoura@gmail.com>
2023-09-21 15:38:01 -0300

    Added files to repository

---
+++ artwork/ContentView.swift
@@ -0,0 +1,77 @@
+//
+//  ContentView.swift
+//  artwork
+//
+//  Created by Mariana Moura de Barros on 21/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            
+            Color(red: 30/255, green: 30/255, blue: 30/255)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 35) {
+                Text("GUSTAV KLIMT")
+                    .font(.system(size:44))
+                    .foregroundColor(Color(red: 214/255, green: 202/255, blue: 134/255))
+                    .bold()
+                HStack {
+                    Image("Art1")
+                        .resizable()
+                        .frame(width: 150, height: 150)
+                        .cornerRadius(10)
+                    VStack {
+                        ZStack{
+                            Image("Art2")
+                                .resizable()
+                                .frame(width: 150, height: 150)
+                                .cornerRadius(10)
+                            Image("x")
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                                .cornerRadius(10)
+                        }
+                        ZStack {
+                            Image("Art3")
+                                .resizable()
+                                .frame(width: 150, height: 150)
+                                .cornerRadius(10)
+                            Image("x")
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                                .cornerRadius(10)
+                            
+                        }
+                    }
+                }
+                
+                
+                Text("Qual é a obra deste artista?")
+                    .font(.headline)
+                    .foregroundColor(Color(red: 129/255, green: 140/255, blue: 151/255))
+                    .bold()
+                
+                Button {
+                    
+                } label: {
+                    Text("Avançar")
+                        .font(.headline)
+                        .foregroundColor(Color(red: 30/255, green: 30/255, blue: 30/255))
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 10)
+                        .background(Color(red: 214/255, green: 202/255, blue: 134/255))
+                        .cornerRadius(20)
+                }
+            }
+        }
+    }
+
+}
+
+#Preview {
+    ContentView()
+}
